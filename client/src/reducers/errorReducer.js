417a9792d46b7554bb5d4d86a6eacbcdee595994
Jhunmark Ng <ngjhunnen@gmail.com>
2018-08-17 17:33:00 +0800

    add error reducer

---
+++ client/src/reducers/errorReducer.js
@@ -0,0 +1,13 @@
+import { GET_ERRORS } from "../actions/types";
+const initialState = {};
+//dispatch actions to this reducer
+export default function(state = initialState, action) {
+  switch (action.type) {
+    // case SET_CURRENT_USER:
+    // return
+    case GET_ERRORS:
+      return action.payload;
+    default:
+      return state;
+  }
+}
