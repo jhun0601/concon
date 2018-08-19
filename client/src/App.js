90059b43715e9415770302cd658a3e72a4c40608
Jhunmark Ng <ngjhunnen@gmail.com>
2018-08-19 18:31:57 +0800

    add if user exists and expiration logout

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
 import store from "./store";
 
 import Navbar from "./components/layout/Navbar";
@@ -10,7 +13,25 @@ import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 
 import "./App.css";
+//check for token
+if (localStorage.jwtToken) {
+  //set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  //decode token and get user information
+  const decoded = jwt_decode(localStorage.jwtToken);
+  //set user and isauthenticated
+  store.dispatch(setCurrentUser(decoded));
 
+  //check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    //logout user
+    store.dispatch(logoutUser());
+    //clear the profile - TODO
+    //redirect to Login
+    window.location.href = "/login";
+  }
+}
 class App extends Component {
   render() {
     return (
