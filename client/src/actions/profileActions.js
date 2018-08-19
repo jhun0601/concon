db159fedfd481d6f1bee2b2e2a3ae2aad9950735
Jhunmark Ng <ngjhunnen@gmail.com>
2018-08-19 23:13:42 +0800

    setup profile

---
+++ client/src/actions/profileActions.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+import {
+  GET_PROFILE,
+  PROFILE_LOADING,
+  //   GET_ERRORS,
+  CLEAR_CURRENT_PROFILE
+} from "./types";
+
+//get current profile
+export const getCurrentProfile = () => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .get("/api/profile")
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: {}
+      })
+    );
+};
+
+//Profile Loading
+
+export const setProfileLoading = () => {
+  return {
+    type: PROFILE_LOADING
+  };
+};
+
+//Profile Clear
+
+export const clearCurrentProfile = () => {
+  return {
+    type: CLEAR_CURRENT_PROFILE
+  };
+};
