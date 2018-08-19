eca436d5757c4de0075f0126c980f77030a79d50
Jhunmark Ng <ngjhunnen@gmail.com>
2018-08-19 19:10:06 +0800

    error reducer

--- client/src/reducers/errorReducer.js
+++ client/src/reducers/errorReducer.js
@@ -1,6 +1,8 @@
 import { GET_ERRORS } from "../actions/types";
+
 const initialState = {};
 //dispatch actions to this reducer
+
 export default function(state = initialState, action) {
   switch (action.type) {
     // case SET_CURRENT_USER:
