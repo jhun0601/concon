5a9c6754e4e959681403417ddbf326ff47b1b981
Jhunmark Ng <ngjhunnen@gmail.com>
2018-08-21 22:40:22 +0800

    add profile all developers

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -5,6 +5,7 @@ import {
   GET_PROFILE,
   PROFILE_LOADING,
   GET_ERRORS,
+  GET_PROFILES,
   CLEAR_CURRENT_PROFILE,
   SET_CURRENT_USER
 } from "./types";
@@ -180,6 +181,24 @@ export const deleteAccount = () => dispatch => {
         );
     }
   });
+};
+
+export const getProfiles = () => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .get("/api/profile/all")
+    .then(res =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: null
+      })
+    );
 };
 
 //Profile Loading
