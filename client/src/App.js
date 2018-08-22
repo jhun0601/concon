31b18d1dd0a49cb05d77f90eb75fedc23e900b2f
Jhunmark Ng <ngjhunnen@gmail.com>
2018-08-22 13:51:14 +0800

    add posts router

--- client/src/App.js
+++ client/src/App.js
@@ -21,6 +21,7 @@ import AddExperience from "./components/add-credentials/AddExperience";
 import AddEducation from "./components/add-credentials/AddEducation";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
+import Posts from "./components/post/Posts";
 import NotFound from "./components/not-found/NotFound";
 
 import "./App.css";
@@ -88,6 +89,9 @@ class App extends Component {
                   path="/add-education"
                   component={AddEducation}
                 />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/feed" component={Posts} />
               </Switch>
               <Route exact path="/not-found" component={NotFound} />
             </div>
