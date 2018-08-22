ab91bcfcf540cf22d711735c0f87e1ba2939eae6
Jhunmark Ng <ngjhunnen@gmail.com>
2018-08-22 13:27:04 +0800

    add profile creds component for profile

--- client/src/components/profile/ProfileCreds.js
+++ client/src/components/profile/ProfileCreds.js
@@ -1,12 +1,88 @@
 import React, { Component } from "react";
+import Moment from "react-moment";
 
 class ProfileCreds extends Component {
   render() {
+    const { experience, education } = this.props;
+    //get experience array
+    const expItems = experience.map(exp => (
+      <li key={exp._id} className="list-group-item">
+        <h4>{exp.company}</h4>
+        <p>
+          <Moment format="MM/YYYY">{exp.from}</Moment> -{" "}
+          {exp.to === null ? (
+            " Now"
+          ) : (
+            <Moment format="MM/YYYY">{exp.to}</Moment>
+          )}
+        </p>
+        <p>
+          <strong>Position: {exp.title}</strong>
+        </p>
+        <p>
+          {exp.location === "" ? null : (
+            <span>
+              <strong>Location: {exp.location}</strong>
+            </span>
+          )}
+        </p>
+        <p>
+          {exp.description === "" ? null : (
+            <span>
+              <strong>Description: {exp.description}</strong>
+            </span>
+          )}
+        </p>
+      </li>
+    ));
+    //get education array
+    const eduItems = education.map(edu => (
+      <li key={edu._id} className="list-group-item">
+        <h4>{edu.school}</h4>
+        <p>
+          <Moment format="MM/YYYY">{edu.from}</Moment> -{" "}
+          {edu.to === null ? (
+            " Now"
+          ) : (
+            <Moment format="MM/YYYY">{edu.to}</Moment>
+          )}
+        </p>
+        <p>
+          <strong>Degree: {edu.degree}</strong>
+        </p>
+        <p>
+          <strong>Field of Study: {edu.fieldofstudy}</strong>
+        </p>
+        <p>
+          {edu.description === "" ? null : (
+            <span>
+              <strong>Description: {edu.description}</strong>
+            </span>
+          )}
+        </p>
+      </li>
+    ));
     return (
-      <div>
-        <h1>TODO: Profile Credentials</h1>
+      <div className="row mt-3">
+        <div className="col-md-6">
+          <h3 className="text-center text-info">Experience</h3>
+          {expItems.length > 0 ? (
+            <ul className="list-group">{expItems}</ul>
+          ) : (
+            <p className="text-center">No Experience listed</p>
+          )}
+        </div>
+        <div className="col-md-6">
+          <h3 className="text-center text-info">Education</h3>
+          {eduItems.length > 0 ? (
+            <ul className="list-group">{eduItems}</ul>
+          ) : (
+            <p className="text-center">No Education listed</p>
+          )}
+        </div>
       </div>
     );
   }
 }
+
 export default ProfileCreds;
