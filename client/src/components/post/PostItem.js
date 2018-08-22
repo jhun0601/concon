0b964d0006bbd156505680b24d38f21299de06a0
Jhunmark Ng <ngjhunnen@gmail.com>
2018-08-22 16:14:07 +0800

    add delete post function

---
+++ client/src/components/post/PostItem.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import classnames from "classnames";
+import { Link } from "react-router-dom";
+import { deletePost } from "../../actions/postActions";
+// import { loginUser } from "../../actions/authActions";
+
+class PostItem extends Component {
+  onDeleteClick(id) {
+    this.props.deletePost(id);
+  }
+
+  render() {
+    const { post, auth } = this.props;
+    return (
+      <div className="card card-body mb-3">
+        <div className="row">
+          <div className="col-md-2">
+            <a href="">
+              <img
+                src={post.avatar}
+                alt=""
+                className="rounded-circle d-none d-md-block"
+              />
+            </a>
+            <br />
+            <p className="text-center">{auth.user.name}</p>
+          </div>
+          <div className="col-md-10">
+            <p className="lead">{post.text}</p>
+
+            <button className="btn btn-light mr-1" type="button">
+              <i className="text-info fas fa-thumbs-up" />
+              <span className="badge badge-light">{post.likes.length}</span>
+            </button>
+
+            <button className="btn btn-light mr-1" type="button">
+              <i className="text-secondary fas fa-thumbs-down" />
+            </button>
+            <Link to={`/post/${post._id}`} className="btn btn-info mr-1">
+              Comments
+            </Link>
+            {post.user === auth.user.id ? (
+              <button
+                type="button"
+                onClick={this.onDeleteClick.bind(this, post._id)}
+                className="btn btn-danger mr-1"
+              >
+                <i className="fas fa-times" />
+              </button>
+            ) : null}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+PostItem.propTypes = {
+  deletePost: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired
+};
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+export default connect(
+  mapStateToProps,
+  { deletePost }
+)(PostItem);
