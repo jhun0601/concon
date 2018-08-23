951bdbccf2a3f357421e1c1bf18176ef11680656
Jhunmark Ng <ngjhunnen@gmail.com>
2018-08-23 08:17:53 +0800

    create server static for heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
-
+const path = requrie("path");
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
@@ -30,6 +30,17 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+
+//server static asset if in production
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static(client / build));
+
+  //create route
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
